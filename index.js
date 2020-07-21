62ab81f5f1eb055c316729d2b6f35de5a0819c4e
ql <qlcola@163.com>
2020-07-21 17:27:39 +0800

    day 1

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-const text = require("./hello");
-document.write(text);
+import { ToyReact, Component } from "./ToyReact.js";
+
+class MyComponent extends Component {
+    render () {
+        return <div>
+            <span>Hello </span>
+            <span>World !</span>
+            <span>{true}</span>
+            <div>{this.children}</div>
+        </div>;
+    }
+}
+
+const a = <MyComponent name="comp-a" id="comp-a">
+    <div>123</div>
+</MyComponent>
+
+ToyReact.render(
+    a,
+    document.body
+);
